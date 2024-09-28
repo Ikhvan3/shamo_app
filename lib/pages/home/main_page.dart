5c2ef366f0329d34d927f6eaf5e9332e77dd52f3
Ikhvan <ikhvankanaya123@gmail.com>
2024-09-28 12:52:55 +0700

    slicing halaman main page - shamoapp

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/pages/home/chat_page.dart';
+import 'package:shamo_app/pages/home/home_page.dart';
+import 'package:shamo_app/pages/home/profile_page.dart';
+import 'package:shamo_app/pages/home/wishlist_page.dart';
 import 'package:shamo_app/theme.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -28,48 +39,85 @@ class MainPage extends StatelessWidget {
           shape: CircularNotchedRectangle(),
           notchMargin: 10,
           clipBehavior: Clip.antiAlias,
-          child: Container(
-            height: 60,
-            color: backgroundColor4,
-            child: BottomNavigationBar(
-              backgroundColor: backgroundColor4,
-              // backgroundColor: Colors.transparent,
-              type: BottomNavigationBarType.fixed,
-              elevation: 0,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Image.asset(
+          child: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (value) {
+              print(value);
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            backgroundColor: backgroundColor4,
+            type: BottomNavigationBarType.fixed,
+            elevation: 0,
+            items: [
+              BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Image.asset(
                     'assets/icon_home.png',
                     width: 21,
+                    color: currentIndex == 0 ? primaryColor : Color(0xff808191),
                   ),
-                  label: '',
                 ),
-                BottomNavigationBarItem(
-                  icon: Image.asset(
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(right: 50),
+                  child: Image.asset(
                     'assets/icon_chat.png',
                     width: 20,
+                    color: currentIndex == 1 ? primaryColor : Color(0xff808191),
                   ),
-                  label: '',
                 ),
-                BottomNavigationBarItem(
-                  icon: Image.asset(
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(left: 50),
+                  child: Image.asset(
                     'assets/icon_whislist.png',
                     width: 20,
+                    color: currentIndex == 2 ? primaryColor : Color(0xff808191),
                   ),
-                  label: '',
                 ),
-                BottomNavigationBarItem(
-                  icon: Image.asset(
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Image.asset(
                     'assets/icon_profile.png',
                     width: 18,
+                    color: currentIndex == 3 ? primaryColor : Color(0xff808191),
                   ),
-                  label: '',
                 ),
-              ],
-            ),
+                label: '',
+              ),
+            ],
           ),
         ),
       );
+    }
+
+    Widget body() {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+          break;
+        case 1:
+          return ChatPage();
+          break;
+        case 2:
+          return WishlistPage();
+          break;
+        case 3:
+          return ProfilePage();
+          break;
+        default:
+          return HomePage();
+      }
     }
 
     return Scaffold(
@@ -77,9 +125,7 @@ class MainPage extends StatelessWidget {
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: customBottomNav(),
-      body: Center(
-        child: Text('Main Page'),
-      ),
+      body: body(),
     );
   }
 }
