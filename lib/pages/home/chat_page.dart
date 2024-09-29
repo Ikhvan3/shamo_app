882d1918ce1a1e0a537927de2e92273aec3b6e4b
Ikhvan <ikhvankanaya123@gmail.com>
2024-09-29 13:52:46 +0700

    slicing home page header - shamoapp

---
+++ lib/pages/home/chat_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Chat Page',
+        style: TextStyle(
+          color: secondaryTextColor,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/home/wishlist_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../theme.dart';
+
+class WishlistPage extends StatelessWidget {
+  const WishlistPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Wishlist Page',
+        style: TextStyle(
+          color: secondaryTextColor,
+        ),
+      ),
+    );
+  }
+}
