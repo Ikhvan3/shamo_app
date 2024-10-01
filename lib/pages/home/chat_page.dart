1bf508757e48d933aeae3a1c780b11bb6d744142
Ikhvan <ikhvankanaya123@gmail.com>
2024-10-01 12:22:13 +0700

    slicing chat page 1 - shamoapp

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -6,13 +6,31 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Chat Page',
-        style: TextStyle(
-          color: secondaryTextColor,
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Message Support',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
         ),
-      ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content() {
+      return Container();
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
