64ab35da3d8924823eb0fecf1bf1192881cb4305
Ikhvan <ikhvankanaya123@gmail.com>
2024-10-03 20:46:33 +0700

    detail chat page bagian chat bubble product preview

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -161,6 +161,7 @@ class DetailChatPage extends StatelessWidget {
           ChatBubble(
             isSender: true,
             text: 'Hi, This item is still available',
+            hasProduct: true,
           ),
           ChatBubble(
             isSender: false,
