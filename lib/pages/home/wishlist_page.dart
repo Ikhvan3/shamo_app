0ace2d579115dd794a5c1b4c410de7ce7dc71372
Ikhvan <ikhvankanaya123@gmail.com>
2024-10-04 01:29:08 +0700

    wishlist page content

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamo_app/widgets/wishlist_card.dart';
 
 import '../../theme.dart';
 
@@ -81,10 +82,28 @@ class WishlistPage extends StatelessWidget {
       );
     }
 
+    Widget content() {
+      return Expanded(
+          child: Container(
+        color: backgroundColor3,
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          children: [
+            WishListCard(),
+            WishListCard(),
+            WishListCard(),
+          ],
+        ),
+      ));
+    }
+
     return Column(
       children: [
         header(),
-        emptyWishlist(),
+        // emptyWishlist(),
+        content(),
       ],
     );
   }
