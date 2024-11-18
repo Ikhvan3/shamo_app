76446dd32082752d9a5935af857bbbfbd7bd5a7d
Ikhvan <ikhvankanaya123@gmail.com>
2024-11-18 14:20:54 +0700

    perbaikan

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:midtrans_sdk/midtrans_sdk.dart';
+import 'package:shamo_app/theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/cart_model.dart';
 
 class TransactionService {
-  String baseUrl = 'http://192.168.1.38:8000/api';
+  String baseUrl = 'http://192.168.1.11:8000/api';
 
   Future<String?> _getToken() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -55,15 +57,46 @@ class TransactionService {
       print('Response body: ${response.body}');
 
       if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        String snapToken = data['snap_token'];
+
+        // Initialize Midtrans SDK
+        await _startMidtransTransaction(snapToken);
         return true;
       } else {
-        print('Server responded with status code: ${response.statusCode}');
-        print('Response body: ${response.body}');
+        print('Error: ${response.statusCode}, ${response.body}');
         return false;
       }
     } catch (e) {
       print('Error during checkout: $e');
       return false;
+    }
+  }
+
+  Future<void> _startMidtransTransaction(String snapToken) async {
+    try {
+      final midtrans = await MidtransSDK.init(
+        config: MidtransConfig(
+          clientKey: 'Mid-client-6RNbPG6IEDvZvHBi',
+          merchantBaseUrl: baseUrl,
+          colorTheme: ColorTheme(
+            colorPrimary: backgroundColor8,
+            colorPrimaryDark: backgroundColor1,
+            colorSecondary: backgroundColor3,
+          ),
+        ),
+      );
+
+      // Start the payment UI flow without expecting a direct result return
+      await midtrans.startPaymentUiFlow(
+        token: snapToken,
+      );
+
+      // Handle the result using some callback or listener method if needed (this will depend on the SDK)
+      // You can listen to the result here after the payment UI flow ends.
+      print('Payment UI flow completed.');
+    } catch (e) {
+      print('Error initializing Midtrans SDK: $e');
     }
   }
 }
