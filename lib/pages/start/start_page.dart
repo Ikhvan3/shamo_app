a2bc5acbaff1ef399e09444be2ca960f5402b9bd
Ikhvan <ikhvankanaya123@gmail.com>
2024-12-04 02:55:01 +0700

    konfigurasi startscreen 2

--- lib/pages/start/start_page.dart
+++ lib/pages/start/start_page.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+import 'startpage1.dart';
+
+class StartScreen extends StatefulWidget {
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentPage = index;
+          });
+        },
+        children: [
+          StartPage1(
+              onNext: () => _pageController.nextPage(
+                  duration: Duration(milliseconds: 300), curve: Curves.ease)),
+          Page2(
+              onNext: () => _pageController.nextPage(
+                  duration: Duration(milliseconds: 300), curve: Curves.ease)),
+          Page3(),
+        ],
+      ),
+    );
+  }
+}
